8297042b99c353c9d84f22366bc4be72c1c2cce1
Omar <gomar@dmacc.edu>
2019-09-17 19:46:15 -0700

    final file

---
+++ validation_with_try/test_average_negative_input.py
@@ -0,0 +1,24 @@
+"""
+Program: validation_with_if.py
+Author: Ghulam Omar
+Last date modified: 09/17/19
+This program the test program for test_validation_with_try.
+"""
+import unittest
+import unittest.mock as result
+from test_validation_with_try import input_validation
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_average(self):
+        with result.patch('builtins.input', side_effect=[-95, -85, -90]):
+            self.assertRaises(ValueError)
+            with result.patch('builtins.input', side_effect=[-40, -44, -56]):
+                self.assertRaises(ValueError)
+                with result.patch('builtins.input', side_effect=[-33, -22, -60]):
+                    self.assertRaises(ValueError)
+
+
+if __name__ == '__main__':
+    unittest.main()
