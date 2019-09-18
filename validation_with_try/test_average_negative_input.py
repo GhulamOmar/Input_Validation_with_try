2527020c5f548dc361f879c4377c5da5bca861d0
Omar <gomar@dmacc.edu>
2019-09-17 20:07:57 -0700

    first test

--- validation_with_try/test_average_negative_input.py
+++ validation_with_try/test_average_negative_input.py
@@ -12,7 +12,7 @@ from test_validation_with_try import input_validation
 class MyTestCase(unittest.TestCase):
 
     def test_average(self):
-        with result.patch('builtins.input', side_effect=[-95, -85, 90]):
+        with result.patch('builtins.input', side_effect=[95, 85, 90]):
             self.assertRaises(ValueError)
 
 
